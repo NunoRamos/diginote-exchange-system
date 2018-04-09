f6d02fd2de6493a721017a69d26360af62366e49
Nuno Ramos <nunor07.unfc@gmail.com>
2018-04-09 12:43:43 +0100

    Fixing OrderNotSatisfiedException deserialization

--- Common/OrderNotSatisfiedException.cs
+++ Common/OrderNotSatisfiedException.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.Serialization;
+using System.Security.Permissions;
 
 namespace Common
 {
@@ -9,12 +10,25 @@ namespace Common
         public int Quantity { get; }
         public float Value { get; }
 
-        public OrderNotSatisfiedException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+        [SecurityPermissionAttribute(SecurityAction.Demand, SerializationFormatter = true)]
+        public OrderNotSatisfiedException(SerializationInfo info, StreamingContext context) : base(info, context) {
+            this.Quantity = info.GetInt32("OrderNotSatisfiedException.Quantity");
+            this.Value = (float)info.GetDouble("OrderNotSatisfiedException.Value");
+        }
 
         public OrderNotSatisfiedException(string message, int quantity, float value) : base(message)
         {
             Quantity = quantity;
             Value = value;
+        }
+
+        [SecurityPermissionAttribute(SecurityAction.Demand, SerializationFormatter = true)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+
+            info.AddValue("OrderNotSatisfiedException.Quantity", this.Quantity);
+            info.AddValue("OrderNotSatisfiedException.Value", this.Value);
         }
     }
 }
