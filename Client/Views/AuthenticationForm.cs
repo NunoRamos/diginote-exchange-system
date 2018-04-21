8d5307520e0cd48a37fc3e59d7b93128cdbb2897
Nuno Ramos <nunor07.unfc@gmail.com>
2018-04-21 23:18:20 +0100

    Removing re-creation of forms

--- Client/Views/AuthenticationForm.cs
+++ Client/Views/AuthenticationForm.cs
@@ -32,7 +32,6 @@ namespace diginote_exchange_system
             }
             else
             {
-                Client.Forms.SystemForm = new SystemForm();
                 Client.Forms.SystemForm.Show();
                 Hide();
             }
@@ -40,7 +39,6 @@ namespace diginote_exchange_system
 
         private void signUpButton_Click(object sender, EventArgs e)
         {
-            Client.Forms.RegistrationForm = new RegistrationForm();
             Client.Forms.RegistrationForm.Show();
             Hide();
         }
